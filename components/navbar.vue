f44fa42fbfc21b70701818f4d6a6172c5deec97e
imzoughene <imzoughene.youssef.01@gmail.com>
2019-12-25 18:08:59 +0100

    good

---
+++ components/navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand">Navbar</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div id="navbarSupportedContent" class="collapse navbar-collapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link">
+            <nuxt-link to="/">Accueil</nuxt-link>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">
+            <nuxt-link to="articles">Posts</nuxt-link>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
---
+++ pages/post/_id.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <h1 class="title">{{ elem.title }}</h1>
+    <p>
+      {{ elem.content }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      posts: [
+        {
+          id: 'rouge',
+          title: 'tortue',
+          content:
+            ' qui quod? Architecto atque dolor ea, inventore molestias nisi'
+        },
+        {
+          id: 'bleu',
+          title: 'loup',
+          content:
+            ' qui quod? Architecto atque dolor ea, inventore molestias nisi'
+        },
+        {
+          id: 'vert',
+          title: 'souris',
+          content:
+            ' qui quod? Architecto atque dolor ea, inventore molestias nisi'
+        }
+      ]
+    }
+  },
+  computed: {
+    elem() {
+      return this.posts.find((post) => post.id === this.id)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
