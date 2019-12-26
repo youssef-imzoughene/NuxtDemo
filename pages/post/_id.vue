eda38a29c751e6a41dcc4ee57a11e376f554f145
imzoughene <imzoughene.youssef.01@gmail.com>
2019-12-26 10:07:18 +0100

    détails page

--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="container">
-    <h1 class="title">{{ elem.title }}</h1>
-    <p>
-      {{ elem.content }}
-    </p>
+    <div class="row">
+      <div class="col-12">
+        <h2>{{ elem.title }}</h2>
+      </div>
+      <div class="col-12">
+        <p>
+          {{ elem.content }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-export default {
-  data() {
-    return {
-      id: this.$route.params.id,
-    }
-  },
-  computed: {
-    elem() {
-      return this.$store.state.post.all.find((post) => post.id === this.id)
+  export default {
+    data() {
+      return {
+        id: this.$route.params.id,
+      }
+    },
+    computed: {
+      elem() {
+        return this.$store.state.post.all.find((post) => post.id === this.id)
+      }
     }
   }
-}
 </script>
 
 <style scoped></style>
